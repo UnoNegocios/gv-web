8826f1b0e6bf77104729421bbc535cd418642c1c
Joel de la Fuente <joeldlf@MacBook-Pro-de-Joel.local>
2022-12-08 22:15:27 -0600

    ogimage test 13

--- pages/perro.vue
+++ pages/perro.vue
@@ -6,13 +6,15 @@
 import axios from 'axios'
 export default {
   name: 'IndexPage',
-  head:{
-    title:'Prueba',
-    meta(){return[{
-      hid: 'og:image', 
-      property: 'og:image', 
-      content: 'https://www.bubblypet.com/wp-content/uploads/2021/08/Shepherd-dog-breed-Australian-Shepherd.jpg'
-    }]}
+  head(){
+    return{
+      title:'Prueba',
+      meta:[{
+        hid: 'og:image', 
+        property: 'og:image', 
+        content: 'https://www.bubblypet.com/wp-content/uploads/2021/08/Shepherd-dog-breed-Australian-Shepherd.jpg'
+      }]
+    }
   }
 }
 </script>
