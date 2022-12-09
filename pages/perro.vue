f812a63684592c71e56a06290fc1043d353681e3
Joel de la Fuente <joeldlf@MacBook-Pro-de-Joel.local>
2022-12-08 23:49:15 -0600

    ogimage test 27

--- pages/perro.vue
+++ pages/perro.vue
@@ -9,13 +9,35 @@ export default {
   async asyncData({ app, route }) {
     let postDetails = await axios.get(`https://gv.unocrm.mx/api/v1/news?filter[slug]=${route.query.n}`);
     postDetails = postDetails.data.data[0];
-    app.head.meta = [{
+    
+    app.head.meta = [/*{
       itemprop: "image",
       hid: 'og:image', 
       rel:"preload",
-      name: 'og:image', 
+      property: 'og:image', 
       content: postDetails.featured_media_path
-    }]
+    },*/
+
+
+
+    {property:"og:site_name", content:"The Rock Photo Studio"},
+    {property:"og:title", content:"The Rock Photo Studio in Florida"},
+    {property:"og:description", content:"The best photo studio for your events"},
+    {property:"og:image", content:"https://www.zooplus.es/magazine/wp-content/uploads/2019/07/border-collie-.jpeg"},
+    {property:"og:type", content:"website"},
+    {property:"og:image:type", content:"image/jpeg"},
+    {property:"og:image:width", content:"300"},
+    {property:"og:image:height", content:"300"},
+    {property:"og:url", content:"http://www.example.com"},
+    
+    
+    
+    
+    
+    
+    
+    
+    ]
     return { postDetails };
   },
 }
