6358d61d5b5d971e2fb45cbc128bd32a6980cd43
Joel de la Fuente <joeldlf@MacBook-Pro-de-Joel.local>
2022-12-08 22:06:50 -0600

    ogimage test 10

--- pages/perro.vue
+++ pages/perro.vue
@@ -1,25 +1,17 @@
 <template>
-  <NuxtLogo/>
+  <div>hola</div>
 </template>
 
 <script>
 import axios from 'axios'
 export default {
   name: 'IndexPage',
-  data(){return{
-    image:''
-  }},
-  created(){
-    axios.get('https://gv.unocrm.mx/api/v1/news?filter[title]=' + this.$route.query.noticia.replace(/porciento/g, '%').replace(/-/g, ' ').replace(/gionmdio/g, '-')).then(resp=>{
-      this.image = resp.data.data[0].featured_media_path
-      console.log(this.image)
-    })
-  },
   head:{
-    //return{
-      title:'Prueba',
-      meta:[{property: 'og:image', content: 'https://www.purina.es/sites/default/files/styles/ttt_image_510/public/2021-02/BREED%20Hero%20Mobile_0026_siberian_husky.jpg?itok=glDRcRov'}]
-    //}
+    title:'Prueba',
+    meta:[{
+      property: 'og:image', 
+      content: 'https://cdn.britannica.com/84/232784-050-1769B477/Siberian-Husky-dog.jpg'
+    }]
   }
 }
 </script>
