fcade4f6c1fa671c5775747c0ed1127042028a0b
Joel de la Fuente <joeldlf@MacBook-Pro-de-Joel.local>
2022-12-08 22:02:09 -0600

    ogimage test 9

--- pages/perro.vue
+++ pages/perro.vue
@@ -18,7 +18,7 @@ export default {
   head:{
     //return{
       title:'Prueba',
-      meta:[{property: 'og:image', content: 'https://gv.unocrm.mx/files/1669855106.png'}]
+      meta:[{property: 'og:image', content: 'https://www.purina.es/sites/default/files/styles/ttt_image_510/public/2021-02/BREED%20Hero%20Mobile_0026_siberian_husky.jpg?itok=glDRcRov'}]
     //}
   }
 }
