5260fb78ec8d379de1a4f0e249441ffb75c78b04
Joel de la Fuente <joeldlf@MacBook-Pro-de-Joel.local>
2022-12-08 23:27:17 -0600

    ogimage test 22

--- pages/perro.vue
+++ pages/perro.vue
@@ -6,25 +6,6 @@
 import axios from 'axios'
 export default {
   name: 'IndexPage',
-  async asyncData({ app, route }) {
-    let postDetails = await app.$axios.get(`https://gv.unocrm.mx/api/v1/news?filter[title]=${encodeURI(route.query.n.replace(/porciento/g, '%').replace(/-/g, ' ').replace(/gionmdio/g, '-'))}`);
-    postDetails = postDetails.data.data[0];
-    const mutation = app.head.meta.map(i => {
-      if(i && i.hid){
-        if(i.hid === 'title'){
-          i.content = postDetails.title
-        }
-        if(i.hid === 'og:image'){
-          i.content = postDetails.featured_media_path
-        }
-      }
-      return i;
-    });
-    app.head.meta = mutation;
-    return { postDetails };
-  },
-}
-/*
   data(){return{
     image:'',
   }},
@@ -44,5 +25,4 @@ export default {
     }
   }
 }
-*/
 </script>
