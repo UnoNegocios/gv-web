5ae51f62e55e0484323f1036be813595074aa36f
Joel de la Fuente <joeldlf@MacBook-Pro-de-Joel.local>
2022-12-09 01:19:45 -0600

    ogimage test 49

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,20 @@
 <template>
   <v-container>
-    <Tutorial/>
+    <SocialHead
+      :title="noticia.data[0].title"
+      :description="noticia.data[0].short_description"
+      :image="noticia.data[0].featured_media_path"
+    />
+    Hola mundo
   </v-container>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  async asyncData({route}) {
+    const noticia = await fetch(`https://gv.unocrm.mx/api/v1/news?filter[slug]=${route.params.n}`).then((res)=>res.json())
+    return { noticia }
+  },
 }
 </script>
