c80fb0b393a8b5c47207b0ae3b07d16d6fd3e315
Joel de la Fuente <joeldlf@MacBook-Pro-de-Joel.local>
2022-12-08 23:35:15 -0600

    ogimage test 23

--- pages/perro.vue
+++ pages/perro.vue
@@ -6,17 +6,29 @@
 import axios from 'axios'
 export default {
   name: 'IndexPage',
+  async asyncData({ app, route }) {
+    let postDetails = await axios.get(`https://gv.unocrm.mx/api/v1/news?filter[slug]=${route.query.n}`);
+    postDetails = postDetails.data.data[0];
+    app.head.meta = [{
+      hid: 'og:image', 
+      property: 'og:image', 
+      content: postDetails.featured_media_path
+    }]
+    return { postDetails };
+  },
+}
+/*
   data(){return{
     image:'',
   }},
   async fetch() {
-    let { data } = await axios.get(`https://gv.unocrm.mx/api/v1/news?filter[title]=${encodeURI(this.$route.query.n.replace(/porciento/g, '%').replace(/-/g, ' ').replace(/gionmdio/g, '-'))}`);
+    let { data } = await axios.get(`https://gv.unocrm.mx/api/v1/news?filter[slug]=${this.$route.query.n}`);
     this.image = data.data[0].featured_media_path
     console.log(this.image)
   },
   head(){
     return{
-      title:this.$route.query.n.replace(/porciento/g, '%').replace(/-/g, ' ').replace(/gionmdio/g, '-'),
+      title:'prueba',
       meta:[{
         hid: 'og:image', 
         property: 'og:image', 
@@ -25,4 +37,5 @@ export default {
     }
   }
 }
+*/
 </script>
