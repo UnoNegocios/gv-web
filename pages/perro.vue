d79e4e0689aec2009db7eb34894b822fd8b31502
Joel de la Fuente <joeldlf@MacBook-Pro-de-Joel.local>
2022-12-08 21:50:52 -0600

    ogimage test 6

--- pages/perro.vue
+++ pages/perro.vue
@@ -3,23 +3,14 @@
 </template>
 
 <script>
-import axios from 'axios'
 export default {
   name: 'IndexPage',
-  data(){return{
-    image:''
-  }},
   created(){
-    axios.get('https://gv.unocrm.mx/api/v1/news?filter[title]=' + this.$route.query.noticia.replace(/porciento/g, '%').replace(/-/g, ' ').replace(/gionmdio/g, '-')).then(resp=>{
-      this.image = resp.data.data[0].featured_media_path
-      console.log(this.image)
-    })
+    console.log(this.$route.query.noticia)
   },
   head:{
-    //return{
-      title:'Prueba',
-      meta:[{property: 'og:image', content: 'https://www.purina.es/sites/default/files/styles/ttt_image_510/public/2021-02/BREED%20Hero%20Mobile_0026_siberian_husky.jpg'}]
-    //}
+    title:'Prueba',
+    meta:[{property: 'og:image', content: 'https://www.purina.es/sites/default/files/styles/ttt_image_510/public/2021-02/BREED%20Hero%20Mobile_0026_siberian_husky.jpg?itok=glDRcRov'}]
   }
 }
 </script>
